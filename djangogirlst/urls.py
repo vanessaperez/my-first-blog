6bf39f69a9fd12cfee186c018ec60c562b74f6d9
Unknown <vane.arc@gmail.com>
2018-10-23 09:17:39 -0400

    Added views to create/edit blog post inside the site.

--- djangogirlst/urls.py
+++ djangogirlst/urls.py
@@ -23,5 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
     path('post/<int:pk>)/', views.post_detail, name='post_detail'),
+    path('post/new', views.post_new, name='post_new'),
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
 
 ]
